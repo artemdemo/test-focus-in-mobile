a0ef8a27b34042e793ecc5b2786016e30e5adc3a
Artem Demo <artemdemo@gmail.com>
2015-11-27 11:19:22 +0300

    Added readme.md

--- js/app.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	'use strict';
-
-	var $homeInput = $('.App-HomeInput');
-
-	$('.App-SetFocus-HomeInput').on('click', function() {
-		$homeInput.focus();
-	});
-})()
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Test custom focus in mobile devices
+
+Code you can find in `gh-pages` branch.
+
+Link to the demo: http://artemdemo.github.io/test-focus-in-mobile/
