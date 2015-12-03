6d2677d8f65f0fbc30015a1bb60a06830ea8cc5a
Artem Demo <artemdemo@gmail.com>
2015-12-03 11:58:22 +0300

    added long textarea

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 (function(){
-	'use strict';
+    'use strict';
 
-	$('.App-SetFocus-HomeInput').on('click', function() {
-		$('.App-HomeInput').focus();
-	});
+    $('.App-SetFocus-HomeInput').on('click', function() {
+        $('.App-HomeInput').focus();
+    });
 
-	$('.App-Open-SecondPage').on('click', function() {
-		$('.SecondPage').html($('#SecondPageTemplate').text());
-		$('.SecondPage').removeClass('hidden');
-		$('.App-SecondPage-TopInput').focus();
-	});
+    $('.App-Open-SecondPage').on('click', function() {
+        $('.SecondPage').html($('#SecondPageTemplate').text());
+        $('.SecondPage').removeClass('hidden');
+        $('.App-SecondPage-TopInput').focus();
+    });
+
+    $('.App-Open-LongTextareaPage').on('click', function() {
+        $('.SecondPage').html($('#LongTextareaPageTemplate').text());
+        $('.SecondPage').removeClass('hidden');
+    });
 
 })()
